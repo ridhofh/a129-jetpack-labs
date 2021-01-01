826a7634cccda8740f806b3633154b92e55db835
Ridho Fahrorozi Hasyim <ridhofahrorozihasyim@gmail.com>
2021-01-01 13:53:17 +0700

    implement live data unit-instrumentation test

--- Project Academy/01. Starter/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
+++ Project Academy/01. Starter/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
@@ -1,5 +1,8 @@
 package com.dicoding.academies.ui.academy
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.dicoding.academies.data.CourseEntity
 import com.dicoding.academies.data.source.AcademyRepository
 import com.dicoding.academies.utils.DataDummy
@@ -7,19 +10,28 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+import kotlin.collections.ArrayList
 
 @RunWith(MockitoJUnitRunner::class)
 class AcademyViewModelTest {
 
     private lateinit var viewModel: AcademyViewModel
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Mock
     private lateinit var academyRepository: AcademyRepository
+
+    @Mock
+    private lateinit var observer: Observer<List<CourseEntity>>
 
     @Before
     fun setUp(){
@@ -28,11 +40,18 @@ class AcademyViewModelTest {
 
     @Test
     fun getCourses() {
-        `when`(academyRepository.getAllCourses()).thenReturn(DataDummy.generateDummyCourses() as ArrayList<CourseEntity>)
-        val courseEntities = viewModel.getCourses()
+        val dummyCourses = DataDummy.generateDummyCourses()
+        val courses = MutableLiveData<List<CourseEntity>>()
+        courses.value = dummyCourses
+
+        `when`(academyRepository.getAllCourses()).thenReturn(courses)
+        val courseEntities = viewModel.getCourses().value
         verify(academyRepository).getAllCourses()
         assertNotNull(courseEntities)
-        assertEquals(5, courseEntities.size)
+        assertEquals(5, courseEntities?.size)
+
+        viewModel.getCourses().observeForever(observer)
+        verify(observer).onChanged(dummyCourses)
     }
 
 
